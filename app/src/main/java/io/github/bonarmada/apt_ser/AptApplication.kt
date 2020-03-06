018e3d6ac6c770a3b332b9bab60b52834525ea1c
vaughn <bonarmada@gmail.com>
2020-03-07 01:00:23 +0800

    Initial commit

---
+++ app/src/main/java/io/github/bonarmada/apt_ser/AptApplication.kt
@@ -0,0 +1,10 @@
+package io.github.bonarmada.apt_ser
+
+import android.app.Application
+
+class AptApplication: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/io/github/bonarmada/apt_ser/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.bonarmada.apt_ser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
